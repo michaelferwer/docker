2c1cd2722d647a2d4665eab850dc52eb5110186b
Werner Michael <michael.ferwer@gmail.com>
2015-06-06 07:23:18 +0200

    make apache2 server with baseimage

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,3 @@
-FROM ubuntu:12.04
+FROM phusion/baseimage:latest
 
-
-RUN apt-get update
-RUN apt-get install -y apache2
-EXPOSE 80
+RUN apt-get install -y apache2 
--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # build image // use --rm to delete intermediaire container
-docker build -f Dockerfile -t test .
+docker build -f Dockerfile -t apache2 .
 # build container and start service apache2    service apache2 start
-docker run --name="apache" -d -p 8080:80 -t test 
+docker run --name="server-apache" -d -p 8080:80 apache2
 docker ps -a
 
 # rejoin a container
