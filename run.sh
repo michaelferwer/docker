8439a6739d9babdbd506a8cd8dc810e95ce5195f
Werner Michael <michael.ferwer@gmail.com>
2015-06-06 17:16:18 +0200

    docker for apache2

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ docker rmi apache2
 # build image // use --rm to delete intermediaire container
 docker build --force-rm=true -f Dockerfile -t apache2 .
 # build container and start service apache2    service apache2 start
-docker run --name="server_apache" -d -p 8080:80 -t apache2
+docker run --name="server_apache" -d -p 8080:80 -v /var/www/html ../jockey -t apache2
 docker ps -a
 
 # if container already exists, start and rejoin the container
